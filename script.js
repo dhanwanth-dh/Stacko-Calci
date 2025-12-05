01e6245b86c849cd550f23ee3746e87cd68ffef0
dhanwanth-dh <dhanwanth.dj.13@gmail.com>
2025-12-05 10:25:06 +0530

    togglebutton_add

--- script.js
+++ script.js
@@ -5,4 +5,28 @@ const num = (num) => {
     const i=document.getElementById("ini")
     i.value+=a
 }
+const back=()=>{
+    const i=document.getElementById("ini")
+    i.value=i.value%10
+}
+function clear(){
+    const l=document.getElementById("ini")
+    l.innerText=""
+}
+let n=0;
+const tog=()=>{
 
+    const cg=document.getElementById("bod")
+    const bu=document.getElementById("tb")
+    if(n==0){
+    cg.classList.add("jackie");
+    bu.classList.add("chan")
+    n++;
+    }
+    else{
+        bu.classList.remove("chan")
+        cg.classList.remove("jackie")
+        n--;
+    }
+}
+ 
